2d731678f1ccf0cdbd349f393d2fd928e48a69e6
fzdwx <likelovec@gmail.com>
2023-02-28 17:41:58 +0800

    feat: new site support sub dir

--- newsite.go
+++ newsite.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"io"
 	"io.github.nollgo/assets"
 	"io/fs"
 	"os"
 	"path/filepath"
 )
 
+const themePath = "theme"
+
 func newSite(path string) error {
 	if path == "" {
 		path = "."
 	}
 
-	dir, err := assets.Dir.ReadDir("theme")
+	dir, err := assets.Dir.ReadDir(themePath)
 	if err != nil {
 		return err
 	}
@@ -20,20 +23,40 @@ func newSite(path string) error {
 	// create dir
 	_ = os.Mkdir(path, 0755)
 
-	for i := range dir {
-		if dir[i].IsDir() {
-			continue
-		}
+	if err = write(path, themePath, dir); err != nil {
+		return err
+	}
+	return nil
+}
 
-		if dir[i].Type() == fs.ModeSymlink {
-			continue
-		}
+func write(path string, parent string, entries []fs.DirEntry) error {
+	for _, entry := range entries {
+		entryPath := filepath.Join(path, entry.Name())
+		if entry.IsDir() {
+			if err := os.Mkdir(entryPath, 0755); err != nil {
+				return err
+			}
+			dir, err := assets.Dir.ReadDir(filepath.Join(parent, entry.Name()))
+			if err != nil {
+				return err
+			}
+			if err = write(entryPath, filepath.Join(parent, entry.Name()), dir); err != nil {
+				return err
+			}
+		} else {
+			f, err := assets.Dir.Open(filepath.Join(parent, entry.Name()))
+			if err != nil {
+				return err
+			}
+			bytes, err := io.ReadAll(f)
+			if err != nil {
+				return err
+			}
 
-		filename := filepath.Join(path, dir[i].Name())
-		if err := os.WriteFile(filename, []byte{}, 0644); err != nil {
-			return err
+			if err = os.WriteFile(entryPath, bytes, 0644); err != nil {
+				return err
+			}
 		}
-
 	}
 	return nil
 }
